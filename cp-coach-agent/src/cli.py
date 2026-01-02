ca17956a88e1cd108c3948af7a25967427aa007f
dev-mir2011 <ruhaanmir.w@gmail.com>
2026-01-02 16:01:49 +0530

    Created Agent and response system

--- cp-coach-agent/src/cli.py
+++ cp-coach-agent/src/cli.py
@@ -19,6 +19,11 @@ group.add_argument("-t", "--text", type=str, metavar='"TEXT"', help="Text input 
 hint_parser = subparsers.add_parser("hint", help="Give a hint")
 hint_parser.add_argument("--level1", "-l1", action="store_true", help="Hint Level One")
 hint_parser.add_argument("--level2", "-l2", action="store_true", help="Hint Level Two")
+hint_parser.add_argument(
+    "--level3", "-l3", action="store_true", help="Hint Level Three"
+)
+hint_parser.add_argument("--level4", "-l4", action="store_true", help="Hint Level Four")
+hint_parser.add_argument("--level5", "-l5", action="store_true", help="Hint Level Five")
 
 
 args = parser.parse_args()
@@ -53,8 +58,17 @@ elif args.command == "hint":
         print(Fore.YELLOW + "[HINT] Level One hint")
     elif args.level2:
         print(Fore.YELLOW + "[HINT] Level Two hint")
+    elif args.level3:
+        print(Fore.YELLOW + "[HINT] Level Three hint")
+    elif args.level4:
+        print(Fore.YELLOW + "[HINT] Level Four hint")
+    elif args.level5:
+        print(Fore.YELLOW + "[HINT] Level Five hint")
     else:
-        print(Fore.RED + "[HINT] No level selected, choose --level1 or --level2")
+        print(
+            Fore.RED
+            + "[HINT] No level selected, choose --level1, --level2, --level3, --level4, --level5"
+        )
 
 else:
     parser.print_help()
